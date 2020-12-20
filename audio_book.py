cddbbf6e3bb6fe22b2a8dedf35a55d3a540caa1b
Pritam <pritamjana360@gmail.com>
2020-12-20 19:32:00 +0530

    version 1.0.0

---
+++ audio_book.py
@@ -0,0 +1,39 @@
+# Python program to show
+import PyPDF2
+# how to convert text to speech
+import pyttsx3
+
+# Initialize the converter
+converter = pyttsx3.init()
+
+# Set properties before adding
+# Things to say
+# Sets speed percent
+# Can be more than 100
+converter.setProperty('rate', 160)
+# Set volume 0-1
+converter.setProperty('volume', 1)
+voices = converter.getProperty('voices')
+converter.setProperty('voice', voices[1].id) #changing index changes voices but ony 0 and 1 are working here
+
+# Open the PDF file
+book = open("estimate of lig building.pdf", "rb")
+pdf_reader = PyPDF2.PdfFileReader(book)
+
+# if pdf contains more then one pages
+pages = pdf_reader.numPages
+print("Total Pages: ", pages)
+
+for page_num in range(pages):
+    print("Current page: ", page_num + 1)
+    page = pdf_reader.getPage(page_num)  # getting each pages from pdf
+
+    text = page.extractText()  # extracting text from current page
+    print(text)
+    converter.say(text)  # now extract text will passed here to listen
+    converter.runAndWait()
+
+# Empties the say() queue
+# Program will not continue
+# until all speech is done talking
+converter.runAndWait()
